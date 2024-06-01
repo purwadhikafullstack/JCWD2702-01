9e0708615d057f8ee6ebc566bbd80522c9934b5b
wulan tsabita <lanabit26@gmail.com>
2024-06-01 23:16:00 +0700

    build: setup

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -19,9 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
-        <Footer />
+        <div className="container">
+          <Header />
+          {children}
+          <Footer />
+        </div>
       </body>
     </html>
   );
--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,3 +1,10 @@
+import { buttonVariants } from './ui/button';
+
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="flex justify-between bg-pink-200 text-lg items-center">
+      <div>Logo</div>
+      <button className={buttonVariants({ variant: 'default' })}>Signup</button>
+    </div>
+  );
 };
