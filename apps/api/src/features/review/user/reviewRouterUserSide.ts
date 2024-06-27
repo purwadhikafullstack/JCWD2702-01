b866ba6ec19f0dff85416704608e423a2de5085d
wulan tsabita <131739585+lanabit@users.noreply.github.com>
2024-06-27 14:31:28 +0700

    reviewRouterUserSide.ts

--- apps/api/src/features/review/user/reviewRouterUserSide.ts
+++ apps/api/src/features/review/user/reviewRouterUserSide.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { allPastStays, postReview } from './reviewControllerUserSide';
 const router = Router();
 
-router.get('/all', allPastStays);
+router.get('/', allPastStays);
 router.post('/', postReview);
 
 export default router;
