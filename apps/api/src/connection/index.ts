114aebca7079c239e443e76398c62ff1e8a9e7e2
wulan tsabita <lanabit26@gmail.com>
2024-07-05 15:33:47 +0700

    chore: connection

--- apps/api/src/connection/index.ts
+++ apps/api/src/connection/index.ts
@@ -6,8 +6,8 @@ import mysql from 'mysql2/promise';
 
 export const mysqlConnection = async () => {
   const connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
   });
